d8043052b4abc41012533072d3b86f8da66e5925
Milutin <milutin91@hotmail.com>
2022-11-23 14:34:22 +0100

    First commit

---
+++ src/main/java/com/example/munchies/MunchiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.munchies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunchiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunchiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/munchies/MunchiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.munchies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunchiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
