e3d2292ee00a731502279862b00944ed3083cefc
Milutin <milutin91@hotmail.com>
2022-11-25 11:48:12 +0100

    Add user and admin employees

---
+++ src/main/resources/db/migration/V2__add_employees_for_testing.sql
@@ -0,0 +1,2 @@
+insert into employee (employee_id, employee_firstname, employee_lastname, employee_email, employee_password, employee_role, employee_created, employee_updated) values (1, 'Suzann', 'Dudgeon', 'sdudgeon0@howstuffworks.com', 'YhWz8NA4PHK', 'USER', '2022-03-06 09:00:00', '2022-03-06 09:00:00');
+insert into employee (employee_id, employee_firstname, employee_lastname, employee_email, employee_password, employee_role, employee_created, employee_updated) values (2, 'Bethina', 'Dulanty', 'bdulanty1@fotki.com', 'Q1WCSY9oIt', 'ADMIN', '2022-11-16 09:00:00', '2022-11-16 09:00:00');
