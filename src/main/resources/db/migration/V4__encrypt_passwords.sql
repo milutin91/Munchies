6edaf57d5d9f39dbafe54b758dbe0843b9a71cf1
Milutin <milutin91@hotmail.com>
2022-11-26 23:17:59 +0100

    Update passwords in employee table

---
+++ src/main/resources/db/migration/V4__encrypt_passwords.sql
@@ -0,0 +1,2 @@
+UPDATE `munchies`.`employee` SET `employee_password` = '$2a$12$t9H/jmZe9Eux73v798C5AeNBREXIy7DbdQt8x10hKxCXspXWoHT9i' WHERE (`employee_id` = '1');
+UPDATE `munchies`.`employee` SET `employee_password` = '$2a$12$OnImt2x1DiJEZyOm1zmLMOhTW1TgHvQT8ZGu3V1gMIkZOV1J0.O0W' WHERE (`employee_id` = '2');
