f5512614e05c4eb1410be0384061e68ba54608de
Milutin <milutin91@hotmail.com>
2022-11-26 23:19:50 +0100

    Create employee repository

--- src/main/java/com/example/munchies/repository/EmployeeRepository.java
+++ src/main/java/com/example/munchies/repository/EmployeeRepository.java
@@ -3,5 +3,10 @@ package com.example.munchies.repository;
 import com.example.munchies.model.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer> {
+
+
+    Optional<EmployeeEntity> findOneByEmployeeEmail(String email);
 }
