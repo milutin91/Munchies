99241173376c428c4212fb718ac9b85f4bc03093
Milutin <milutin91@hotmail.com>
2022-12-20 19:33:24 +0100

    Fix validation error messages for selection

--- src/main/java/com/example/munchies/controller/OrderItemController.java
+++ src/main/java/com/example/munchies/controller/OrderItemController.java
@@ -1,13 +1,17 @@
 package com.example.munchies.controller;
 
+import com.example.munchies.ExceptionHandling.GroupOrderTimeoutException;
+import com.example.munchies.ExceptionHandling.NotFoundException;
 import com.example.munchies.model.dto.GroupOrderDTO;
 import com.example.munchies.model.dto.OrderItemCreationDTO;
 import com.example.munchies.service.GroupOrderService;
 import com.example.munchies.service.OrderItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -21,16 +25,20 @@ public class OrderItemController {
     @Autowired
     GroupOrderService groupOrderService;
 
-    // TODO: 12/20/2022 fix not displaying validation messages
-    @PostMapping("/restaurant/group-order/selection-create")
-    public String createEmployeeSelection(@ModelAttribute("orderItemCreation") @Valid OrderItemCreationDTO orderItemCreationDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
-        GroupOrderDTO groupOrderDTO = groupOrderService.getLatestGroupOrderDTO();
+    @PostMapping("/restaurant/group-order/{groupOrderId}/selection")
+    public String createEmployeeSelection(@PathVariable("groupOrderId") Integer id,
+                                          @ModelAttribute("orderItemCreation") @Valid OrderItemCreationDTO orderItemCreationDTO,
+                                          BindingResult bindingResult,
+                                          RedirectAttributes redirectAttributes,
+                                          Model model) throws GroupOrderTimeoutException, NotFoundException {
+        GroupOrderDTO groupOrderDTO = groupOrderService.getGroupOrder(id);
         redirectAttributes.addAttribute("groupOrderId", groupOrderDTO.getGroupOrderId());
-        if(bindingResult.hasErrors()){
-            return "redirect:/restaurant/group-order/{groupOrderId}";
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("groupOrder", groupOrderDTO);
+            model.addAttribute("orderItemCreation", orderItemCreationDTO);
+            return "group_order";
         }
-
-                orderItemService.createEmployeeSelection(orderItemCreationDTO);
+        orderItemService.createEmployeeSelection(id, orderItemCreationDTO);
         return "redirect:/restaurant/group-order/{groupOrderId}";
     }
 }
